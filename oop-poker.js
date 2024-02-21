e50eb50c112584527d8ca7296964660bcde02cb2
julia.paulin <julia.paulin@chasacademy.se>
2024-02-21 23:31:11 +0100

    started on poker

---
+++ oop-poker.js
@@ -0,0 +1,322 @@
+class Deck {
+  //Constructor
+  constructor(cardDeck) {
+    this.cardDeck = cardDeck;
+    this.cards = [];
+  }
+
+  //Methods
+  addCard(card) {
+    this.cards.push(card);
+  }
+
+  displayCardDeck() {
+    return `Deck: ${this.cards.join(", ")}`;
+  }
+}
+
+class Card {
+  constructor(name, color, number) {
+    this.name = name;
+    this.color = color;
+    this.number = number;
+  }
+
+  addColor(color) {
+    this.color.push(color);
+  }
+
+  displayCard() {
+    return `Card: ${this.name} ${this.color} ${this.number}`;
+  }
+}
+
+//Create new object
+const myDeck = new Deck("The deck");
+const card1 = new Card("Diamond", "red", 2);
+myDeck.addCard(card1);
+const card2 = new Card("Heart", "red", 2);
+myDeck.addCard(card2);
+const card3 = new Card("Club", "black", 2);
+myDeck.addCard(card3);
+const card4 = new Card("Spade", "black", 2);
+myDeck.addCard(card4);
+
+const card5 = new Card("Diamond", "red", 3);
+myDeck.addCard(card5);
+const card6 = new Card("Heart", "red", 3);
+myDeck.addCard(card6);
+const card7 = new Card("Club", "black", 3);
+myDeck.addCard(card7);
+const card8 = new Card("Spade", "black", 3);
+myDeck.addCard(card8);
+
+const card9 = new Card("Diamond", "red", 4);
+myDeck.addCard(card9);
+const card10 = new Card("Heart", "red", 4);
+myDeck.addCard(card10);
+const card11 = new Card("Club", "black", 4);
+myDeck.addCard(card11);
+const card12 = new Card("Spade", "black", 4);
+myDeck.addCard(card12);
+
+const card13 = new Card("Diamond", "red", 5);
+myDeck.addCard(card13);
+const card14 = new Card("Heart", "red", 5);
+myDeck.addCard(card14);
+const card15 = new Card("Club", "black", 5);
+myDeck.addCard(card15);
+const card16 = new Card("Spade", "black", 5);
+myDeck.addCard(card16);
+
+const card17 = new Card("Diamond", "red", 6);
+myDeck.addCard(card17);
+const card18 = new Card("Heart", "red", 6);
+myDeck.addCard(card18);
+const card19 = new Card("Club", "black", 6);
+myDeck.addCard(card19);
+const card20 = new Card("Spade", "black", 6);
+myDeck.addCard(card20);
+
+const card21 = new Card("Diamond", "red", 7);
+myDeck.addCard(card21);
+const card22 = new Card("Heart", "red", 7);
+myDeck.addCard(card22);
+const card23 = new Card("Club", "black", 7);
+myDeck.addCard(card23);
+const card24 = new Card("Spade", "black", 7);
+myDeck.addCard(card24);
+
+const card25 = new Card("Diamond", "red", 8);
+myDeck.addCard(card25);
+const card26 = new Card("Heart", "red", 8);
+myDeck.addCard(card26);
+const card27 = new Card("Club", "black", 8);
+myDeck.addCard(card27);
+const card28 = new Card("Spade", "black", 8);
+myDeck.addCard(card28);
+
+const card29 = new Card("Diamond", "red", 9);
+myDeck.addCard(card29);
+const card30 = new Card("Heart", "red", 9);
+myDeck.addCard(card30);
+const card31 = new Card("Club", "black", 9);
+myDeck.addCard(card31);
+const card32 = new Card("Spade", "black", 9);
+myDeck.addCard(card32);
+
+const card33 = new Card("Diamond", "red", 10);
+myDeck.addCard(card33);
+const card34 = new Card("Heart", "red", 10);
+myDeck.addCard(card34);
+const card35 = new Card("Club", "black", 10);
+myDeck.addCard(card35);
+const card36 = new Card("Spade", "black", 10);
+myDeck.addCard(card36);
+
+const card37 = new Card("Diamond,Jack", "red", 11);
+myDeck.addCard(card37);
+const card38 = new Card("Heart,Jack", "red", 11);
+myDeck.addCard(card38);
+const card39 = new Card("Club,Jack", "black", 11);
+myDeck.addCard(card39);
+const card40 = new Card("Spade,Jack", "black", 11);
+myDeck.addCard(card40);
+
+const card41 = new Card("Diamond,Queen", "red", 12);
+myDeck.addCard(card41);
+const card42 = new Card("Heart,Queen", "red", 12);
+myDeck.addCard(card42);
+const card43 = new Card("Club,Queen", "black", 12);
+myDeck.addCard(card43);
+const card44 = new Card("Spade,Queen", "black", 12);
+myDeck.addCard(card44);
+
+const card45 = new Card("Diamond,King", "red", 13);
+myDeck.addCard(card45);
+const card46 = new Card("Heart,King", "red", 13);
+myDeck.addCard(card46);
+const card47 = new Card("Club,King", "black", 13);
+myDeck.addCard(card47);
+const card48 = new Card("Spade,King", "black", 13);
+myDeck.addCard(card48);
+
+const card49 = new Card("Diamond,Ace", "red", 14);
+myDeck.addCard(card49);
+const card50 = new Card("Heart,Ace", "red", 14);
+myDeck.addCard(card50);
+const card51 = new Card("Club,Ace", "black", 14);
+myDeck.addCard(card51);
+const card52 = new Card("Spade,Ace", "black", 14);
+myDeck.addCard(card52);
+//console.log(myDeck);
+
+//Shuffles the card deck------------------------------------------------
+const shuffle = (deck) => {
+  return deck
+    .map((a) => ({ sort: Math.random(), value: a }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value);
+};
+const shuffledDeck = shuffle(myDeck.cards);
+
+//---------------------------------------------------------------------------
+
+class Player {
+  //Constructor
+  constructor(name) {
+    this.name = name;
+    this.cards = [];
+  }
+  //Methods
+
+  addCards(card) {
+    this.cards.push(card);
+  }
+  removeCards(card) {
+    this.cards.push(card);
+  }
+
+  displayPlayer() {
+    return `Player: ${this.name} ${this.cards}`;
+  }
+}
+
+let player1 = new Player("Slim");
+let player2 = new Player("Luke");
+
+// Deal cards to player and display the rest------------------------------------------
+const dealCards = (deck, player) => {
+  const dealed = deck
+    .map((a) => ({ sort: Math.random(), value: a }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .slice(0, 5);
+
+  dealed.forEach((card) => {
+    player.addCards(card);
+  });
+
+  let newDeck = deck;
+
+  dealed.forEach((dealedCard) => {
+    newDeck = newDeck.filter((deckCard) => deckCard !== dealedCard);
+  });
+  return newDeck;
+};
+
+const leftOfDeck = dealCards(myDeck.cards, player1);
+//console.log(player1);
+//console.log(leftOfDeck);
+
+const leftOfdealedTwo = dealCards(leftOfDeck, player2);
+//console.log(player2);
+//console.log(leftOfdealedTwo);
+
+//Count total amount on hands-------------------------------------------------------
+const countCardsAmount = (player1, player2) => {
+  const totalHandValue = [...player1.cards, ...player2.cards];
+  console.log(totalHandValue);
+
+  const total = totalHandValue.reduce((acc, item) => {
+    const cardValue = parseInt(item.number) || 0;
+    return acc + cardValue;
+  }, 0);
+  return total;
+};
+
+//console.log(countCardsAmount(player1, player2));
+
+//Throw two cards, get two new ones----------------------------------------------------
+const playerHand = (player) => {
+  return (leftOfCards = player.cards.splice(0, 2));
+};
+
+const throwCardsPlayer1 = playerHand(player1);
+const throwCardsPlayer2 = playerHand(player2);
+//console.log(player1);
+//console.log(player2);
+
+//Deal two new cards----------------------------------------------------------
+const dealTwoNewCards = (deck, player) => {
+  const dealed = deck
+    .map((a) => ({ sort: Math.random(), value: a }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .slice(0, 2);
+
+  dealed.forEach((card) => {
+    player.addCards(card);
+  });
+
+  let newDeck = deck;
+
+  dealed.forEach((dealedCard) => {
+    newDeck = newDeck.filter((deckCard) => deckCard !== dealedCard);
+  });
+  return newDeck;
+};
+
+const twoNewCards1 = dealTwoNewCards(leftOfdealedTwo, player1);
+//console.log(player1);
+//console.log(twoNewCards1);
+
+const twoNewCards2 = dealTwoNewCards(twoNewCards1, player2);
+//console.log(player2);
+//console.log(twoNewCards2);
+
+//Total sum of all cards on hand - round two-------------------------------------------------
+//console.log(countCardsAmount(player1, player2));
+
+//Throw all cards------------------------------------------------------------------
+
+//---------------------------------------------------------------------
+
+/* const SUITS = ["spades", "hearts", "diamond", "club"];
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+
+class Deck {
+  constructor(cards = freshDeck()) {
+    this.cards = cards;
+  }
+  shuffle(){
+    for (let i = this.cards.length -1; i > 0; i--) {
+        const newIndex = Math.floor(Math.random() * (i + 1))
+        const oldValue = this.cards[newIndex]
+        this.cards[newIndex] = this.cards[i]
+        this.cards[i] = oldValue
+    }
+  }
+}
+class Card {
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+  }
+}
+
+function freshDeck() {
+  return SUITS.flatMap((suit) => {
+    return VALUES.map((value) => {
+      return new Card(suit, value);
+    });
+  });
+}
+
+const deck = new Deck();
+deck.shuffle()
+console.log(deck); */
--- script.js
+++ script.js
@@ -3,16 +3,16 @@ const hexButton = document.querySelector(".hex-button");
 const rgbaButton = document.querySelector(".rgba-button");
 const background = document.querySelector(".background");
 
-//Kontrukturfunktion
+// Konstrukturfunktion
 function Color(r, g, b) {
-  this.r = r; // name sätts till det värde vi skickar med när vi skapar instansen/objektet när vi kallar på konstrukturfunktion
+  this.r = r;
   this.g = g;
   this.b = b;
 }
 
-//Skapa metoder
+// Metoder
 Color.prototype.rgb = function () {
-  return `rgb${(this.r, this.g, this.b)}`;
+  return `rgb(${this.r}, ${this.g}, ${this.b})`;
 };
 
 Color.prototype.hex = function () {
@@ -26,17 +26,20 @@ Color.prototype.rgba = function (alpha) {
   return `rgba(${this.r}, ${this.g}, ${this.b}, ${alpha})`;
 };
 
-//Skapar instanser av objektet
-const red = new Color(255, 0, 0); //Skapar instansen/objektet med dess grund
-const green = new Color(0, 128, 0);
+// Instanser av objektet
+const yellow = new Color(232, 186, 58);
+const green = new Color(17, 120, 36);
 const blue = new Color(8, 143, 143);
-
-console.log(red.hex());
-console.log(green.hex());
 
 rgbButton.addEventListener(
   "click",
-  () => (background.style.background = red.rgb())
+  () => (background.style.background = yellow.rgb())
 );
-hexButton.addEventListener("click", () => green.hex());
-rgbaButton.addEventListener("click", () => blue.rgba());
+hexButton.addEventListener(
+  "click",
+  () => (background.style.background = green.hex())
+);
+rgbaButton.addEventListener(
+  "click",
+  () => (background.style.background = blue.rgba(0.4))
+);
